45c2113034758973f0cb2c71793a293f10b12996
ysut <yshutn@gmail.com>
2024-06-03 20:24:36 +0900

    update

--- IDconvert/biomart.R
+++ IDconvert/biomart.R
@@ -7,7 +7,7 @@ ids = c("hgnc_symbol", "hgnc_id", "entrezgene_id", "description")
 
 listDatasets(db)
 filters <- listFilters(hd)
-filters[grep("symbol", filters[,1]),]
+filters[grep("transcript", filters[,1]),]
 
 # hgnc_symbol
 # hgnc_id
@@ -50,17 +50,35 @@ res4 <- getBM(attributes = ids,
               mart = hd, useCache = FALSE)
 
 res4
+write.table(res4, "phaplo.tsv", sep = "\t", row.names = FALSE)
 
 ################################################################################
+data <- read.xlsx("genovo.xlsx", 1)
+ids = c("ensembl_transcript_id", "hgnc_symbol", "hgnc_id", "entrezgene_id", "description")
+res5 <- getBM(attributes = ids,
+              filters = "ensembl_transcript_id", values = data$enstID, 
+              mart = hd, useCache = FALSE)
+
+res5
+write.table(res5, "genovo.tsv", sep = "\t", row.names = FALSE)
 
 
+################################################################################
+data <- read.xlsx("gnocchi.xlsx", 1)
+ids = c("hgnc_symbol", "hgnc_id", "entrezgene_id", "description")
+res6 <- getBM(attributes = ids,
+              filters = "hgnc_symbol", values = data$gene, 
+              mart = hd, useCache = FALSE)
+res6
+write.table(res6, "gnocchi.tsv", sep = "\t", row.names = FALSE)
 
-
-
-
-
-
-
-
+################################################################################
+data <- read.xlsx("am.xlsx", 1)
+ids = c("hgnc_symbol", "hgnc_id", "entrezgene_id", "description")
+res7 <- getBM(attributes = ids,
+              filters = "hgnc_symbol", values = data$gene, 
+              mart = hd, useCache = FALSE)
+res7
+write.table(res7, "am.tsv", sep = "\t", row.names = FALSE)
 
 
