177e8423cbda21dc43f2aec18453e22af73985f4
ysut <yshutn@gmail.com>
2024-06-23 16:25:46 +0900

    update

--- genome_plot/Case_ZNF516.R
+++ genome_plot/Case_ZNF516.R
@@ -3,8 +3,15 @@ library(BSgenome.Hsapiens.UCSC.hg19)
 library(openxlsx)
 library(rtracklayer)
 
-# Conncecting options
-options(Gviz.ucscUrl="http://genome-asia.ucsc.edu/cgi-bin/")
+## Tracks & Tables
+session <- browserSession()
+genome(session) <- "hg19"
+query <- ucscTableQuery(session, "Fix Patches")
+trackNames(query)
+tableNames(query)
+
+# Conncecting options 
+# options(Gviz.ucscUrl="http://genome-asia.ucsc.edu/cgi-bin/")
 
 # Genome assembly
 gen <- "hg19"
@@ -24,7 +31,6 @@ input_bam <- znf516_bam
 scores_xlsx <- znf516_xlsx
 
 
-
 ## CCRs
 ccrs_bw <- "bigwig/ccrs.autosomes.v2.20180420.bw"
 grangesData <- import(ccrs_bw, format = "bigWig")
@@ -34,26 +40,25 @@ lowScoreData <- grangesData[grangesData$score < 90]
 
 highScoreTrack <- DataTrack(
   range = highScoreData, type = "histo", chromosome = chr, genome = gen,
-  name = "CCRs (%tile)", fill = "#EF8875", ylim = c(0, 100)
+  name = "CCRs", fill = "#EF8875", ylim = c(0, 100)
 )
 
 mediumScoreTrack <- DataTrack(
   range = mediumScoreData, type = "histo", chromosome = chr, genome = gen,
-  name = "CCRs (%tile)", fill = "#F7CDB6", ylim = c(0, 100), 
-  fontsize = 1.2, size = 0.5, cex.title = 10,
+  name = "CCRs", fill = "#F7CDB6", ylim = c(0, 100), 
+  fontsize = 10, size = 24, cex.title = 1, cex.axis = 0.5
 )
-
 lowScoreTrack <- DataTrack(
   range = lowScoreData, type = "histo", chromosome = chr, genome = gen,
-  name = "CCRs (%tile)", fill = "lightgrey", ylim = c(0, 100), alpha = 0.3
+  name = "CCRs (%tile)", fill = "lightgrey", ylim = c(0, 100), alpha = 0.5
 )
 ov = OverlayTrack(
   list(mediumScoreTrack, lowScoreTrack, highScoreTrack),
-  name = "CCRs (%tile)", background.title = "#909090"
+  name = "CCRs", background.title = "#909090"
   )
 
 plotTracks(
-  list(iTrack, ht, ov, axTrack), from = 74153000, to = 74156000
+  list(ov), from = from, to = to
 )
 
 #####################
@@ -83,25 +88,47 @@ for (i in 1:length(start(ucscTrack))) {
 displayPars(ucscTrack) <- list(
   background.title = "#404040", fill ="#665990", cex = 1, cex.group = 1.2,
   transcriptAnnotation = "transcript",
-  fontsize = 1.2, size = 0.5, cex.title = 10,
+  fontsize = 3, size = 0.2, cex.title = 2.5,
   rotation.title = 0
 )
 
+
 # Highlight the variant position
-ht <- HighlightTrack(ucscTrack, alpha = 0.5, inBackground = FALSE, 
-  chromosome = chr, 
-  start = c(pos - 80, 4920300), width = c(160, 100)
+ht <- HighlightTrack(list(ucscTrack), alpha = 0.5, inBackground = FALSE, 
+  chromosome = chr,
+  start = c(pos - 80, 74074450), width = c(160, 100)
 )
+
+# Wide view with highlighted variant position
+plotTracks(
+  list(iTrack, ht, axTrack), from = from - 22000, to = to + 1000)
+
 # ideogram
 iTrack <- IdeogramTrack(
   genome = gen, chromosome = chr, 
   cex = 1.2, bevel = 1, showId = FALSE, size = 0.3, cex.bands = 1.2
 )
-# Wide view with highlighted variant position
-plotTracks(
-  list(iTrack, ht, ov, axTrack), from = from - 15000, to = to + 1000
+
+### 2nd wide
+# Highlight the variant position
+ht2 <- HighlightTrack(list(ucscTrack, ov), alpha = 0.5, inBackground = FALSE, 
+                     chromosome = chr,
+                     start = c(pos - 80, 74074450), width = c(160, 100)
 )
 
+# Wide view with highlighted variant position
+plotTracks(
+  list(ht2), from = 74070000, to = 74070000 + 14000)
+
+
+# chr18:74074452-74074513 and chr18:74082483-74082524
+idrTrack <- AnnotationTrack(
+  genome = gen, width = c(60, 11), name = 'IDR', fill = "darkgrey", 
+  background.title = "#404040", shape = "box", group = "IDR", 
+  just.group = "right", showOverplotting = TRUE, showTitle = TRUE,
+  chromosome = chr, start = c(74074452, 74082483), size = 12, 
+  rotation.title = 90, fontsize = 5, cex.title = 2.5
+)
 
 ##########################
 ##### Zoomed in view #####
@@ -109,7 +136,7 @@ plotTracks(
 # axis
 axTrack <- GenomeAxisTrack(
   add35 = FALSE, add53 = FALSE, exponent = 0, fontcolor = "#383838",
-  fontsize = 20, size = 12, distFromAxis = 1.5
+  fontsize = 12, size = 16, distFromAxis = 1.5
 )
 
 zoomucscTrack <- UcscTrack(
@@ -124,14 +151,14 @@ for (i in 1:length(start(zoomucscTrack))) {
 
 displayPars(zoomucscTrack) <- list(
   background.title = "#404040", fill ="#665990", cex = 1.0, showTitle = FALSE,
-  name = "Gene", cex.title = 6, fontsize = 4, cex.axis = 4, rotation.title = 0,
+  name = "Gene", cex.title = 0.5, fontsize = 4, cex.axis = 1, rotation.title = 0,
   size = 6
 )
 
 ##
 fontcolor = c(A = "#49A190", C = "#0072B2", G = "#1E181B", T = "red", N = 1)
 sTrack <- SequenceTrack(
-  Hsapiens, fontcolor = fontcolor, fontsize = 12, 
+  Hsapiens, fontcolor = fontcolor, fontsize = 6, 
   size = 5, 
   ) 
 
@@ -144,10 +171,9 @@ splTrack <- DataTrack(
   ylim = c(-1, 1), lwd.baseline = 2,
   yTicksAt = c(-1.0, -0.5,0 , 0.5, 1.0), groups = c("AG", "AL", "DG", "DL"),
   col = c("#6088C6", "#EF8875", "#49A190", "#ED8D49"), legend = FALSE, 
-  cex.legend = 2, cex.title = 6, fontsize = 6, cex.axis = 4, 
+  cex.legend = 2, cex.title = 1, fontsize = 12, cex.axis = 1, 
   size = 55
   )
-
 
 ##
 options(ucscChromosomeNames=FALSE) 
@@ -156,12 +182,12 @@ alTrack <- AlignmentsTrack(
   background.title = "darkgrey", type = c("pileup"), 
   stacking = "squish", name = "WES reads", showAxis = FALSE,
   max.height = 10, min.height = 0.1, lwd.mismatch = 0.1, alpha.reads = 0.75, 
-  col.axis = "darkgray", cex.title = 6, fontsize = 6, cex.axis = 4, 
+  col.axis = "darkgray", cex.title = 1, fontsize = 10, cex.axis = 1, 
   size = 25
 )
 
-plotTracks(c(axTrack, zoomucscTrack, splTrack, sTrack, alTrack), 
-           from = pos - 100, to = pos + 5, chromosome = chr
+plotTracks(c(axTrack, zoomucscTrack, splTrack, sTrack, alTrack, ov, idrTrack), 
+           from = pos - 75, to = pos + 7, chromosome = chr
            )
 
 #################
@@ -174,7 +200,7 @@ alTrack <- AlignmentsTrack(
   background.title = "darkgrey", type = c("pileup"), 
   stacking = "squish", name = "WES reads", showAxis = FALSE,
   max.height = 10, min.height = 0.1, lwd.mismatch = 0.1, alpha.reads = 0.75, 
-  col.axis = "darkgray", cex.title = 12, fontsize = 2, cex.axis = 3, 
+  col.axis = "darkgray", cex.title = 1, fontsize = 14, cex.axis = 1, 
   size = 20
 )
 
@@ -186,17 +212,10 @@ sTrack <- SequenceTrack(
 ptcTrack <- AnnotationTrack(
     genome = gen, width = 2, name = 'PTC', fill = "red", 
     background.title = "#404040", shape = "box", group = "PTC", 
-    just.group = "right", showOverplotting = TRUE,
-    chromosome = chr, start = 4920336, size = 5, rotation.title = 0, fontsize = 24
+    just.group = "right", showOverplotting = TRUE, showTitle = FALSE,
+    chromosome = chr, start = 74074496, size = 5, rotation.title = 0, fontsize = 8
 )
 
-plotTracks(c(axTrack, zoomucscTrack, ptcTrack, sTrack, alTrack), 
-           from = 4920325, to = 4920350, chromosome = chr
+plotTracks(c(axTrack, zoomucscTrack, ptcTrack, sTrack, alTrack, ov, idrTrack), 
+           from = 74074486, to = 74074515, chromosome = chr
 )
-
-
-##### CCRs
-
-
-plotTracks(c(sTrack, ccrTrack), from = pos -100, to = pos +100, chromosome = chr)
-
