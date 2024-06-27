c14ff1c0dacf2eb45e7a4f0a31f200911d24b353
ysut <yshutn@gmail.com>
2024-06-27 15:56:06 +0900

    update

--- genome_plot/situations/situations.R
+++ genome_plot/situations/situations.R
@@ -9,21 +9,20 @@ chr <- "chr1"
 ###  Axis Track  ###
 axTrack <- GenomeAxisTrack(
   add35 = FALSE, add53 = FALSE, exponent = 0, fontcolor = "#383838",
-  fontsize = 10, labelPos = "above", size = 3, distFromAxis = 1.2
+  fontsize = 10, labelPos = "above", size = 10, distFromAxis = 1.2
 )
 
 ###  Dummy scores for overlap track ###
 dummyscores <- read.xlsx("dummy_scores.xlsx")
 dummyTrack <- DataTrack(
   range = dummyscores, chromosome = chr, genome = gen, name="SpliceAI ∆Score", 
-  background.title = "#F8ACAC", type = "histogram", 
+  background.title = "#F8ACAC", type = "histogram", ylim = c(-1, 1),
   baseline = 0, col.baseline = "#838383", lty.baseline = 1, lwd.baseline = 1,
-  ylim = c(-1, 1),
   yTicksAt = c(-1.0, 0, 1.0), groups = c("AG", "AL", "DG", "DL"),
   col = c("#6088C6", "#EF8875", "#49A190", "#ED8D49"), legend = FALSE, 
-  cex.title = 1.5, 
-  fontsize = 12, cex.axis = 1.5, 
-  size = 1
+  cex.title = 1.2, 
+  fontsize = 12, cex.axis = 1.2, 
+  size = 2
 )
 
 #####################
@@ -34,64 +33,148 @@ dummyTrack <- DataTrack(
 #5. Partial intron retention
 
 #1. Pseudo exon activation
-###  Gene model  ###
-gene_model_xlsx <- "situations.xlsx"
+gene_model_xlsx <- "01_pseudoexon/model_base.xlsx"
 genemodel <- read.xlsx(gene_model_xlsx)
 gmtrack <- GeneRegionTrack(
   genemodel, name = "Gene Model", background.title = "#665990", fill = "#383838",
   chromosome = chr, genome = "hg19", size = 0.1, alpha = 0.70)
 
-pseudoexon_xlsx <- "pseudoexon.xlsx"
+pseudo_xlsx <- "01_pseudoexon/model_pseudoexon.xlsx"
+pseudomodel <- read.xlsx(pseudo_xlsx)
+pseudoexonTrack <- GeneRegionTrack(
+  pseudomodel, name = "Gene Model", background.title = "#665990", fill = "red",
+  chromosome = chr, genome = "hg19", size = 0.3, alpha = 0.2)
+
+pseudoexon_xlsx <- "01_pseudoexon/splai_pseudoexon.xlsx"
 pseudoexon_scores <- read.xlsx(pseudoexon_xlsx)
 pseudoex_splaiTrack <- DataTrack(
   range = pseudoexon_scores, chromosome = chr, genome = gen, name="SpliceAI ∆Score", 
   background.title = "#F8ACAC", type = "histogram", 
-  ylim = c(-1, 1), lwd.baseline = 2,
-  yTicksAt = c(-1.0, 0, 1.0), groups = c("AG", "AL", "DG", "DL"),
+  ylim = c(-1, 1), yTicksAt = c(-1, 0, 1),
+  # baseline = 0, col.baseline = "#838383", lty.baseline = 1, lwd.baseline = 1,
+  groups = c("AG", "AL", "DG", "DL"),
   col = c("#6088C6", "#EF8875", "#49A190", "#ED8D49"), legend = FALSE, 
-  cex.legend = 2, cex.title = 1.5, 
-  fontsize = 12, cex.axis = 1.5, 
   size = 1
 )
 
-
-pseudo_xlsx <- "pseudo.xlsx"
-pseudomodel <- read.xlsx(pseudo_xlsx)
-pseudoexonTrack <- GeneRegionTrack(
-  pseudomodel, name = "Gene Model", background.title = "#665990", fill = "red",
-  chromosome = chr, genome = "hg19", size = 0.3, alpha = 0.3)
-
+ov <- OverlayTrack(
+  list(dummyTrack, gmtrack, pseudoex_splaiTrack, pseudoexonTrack), 
+  background.title = "#665990"
+)
+plotTracks(c(axTrack, ov), from = 985, to = 1155)
 
 
 
-### SpliceAI
-exonskip_xlsx <- "skip.xlsx"
-exonskip_scores <- read.xlsx(exonskip_xlsx)
-skipTrack <- DataTrack(
+#2. Exon skipping
+genemodel <- read.xlsx("02_exskip/model_base_skip.xlsx")
+gmtrack <- GeneRegionTrack(
+  genemodel, fill = "#383838",
+  chromosome = chr, genome = "hg19", size = 0.1, alpha = 0.70)
+
+skipex <- read.xlsx("02_exskip/model_exonskip.xlsx")
+skipexTrack <- GeneRegionTrack(
+  skipex, fill = "#383838", chromosome = chr, genome = "hg19", 
+  size = 0.3, alpha = 0.2)
+
+exonskip_scores <- read.xlsx("02_exskip/splai_exonskip.xlsx")
+exonskip_splaiTrack <- DataTrack(
   range = exonskip_scores, chromosome = chr, genome = gen, name="SpliceAI ∆Score", 
   background.title = "#F8ACAC", type = "histogram", 
-  baseline = 0, col.baseline = "#838383", lty.baseline = 2, lwd.baseline = 2,
-  ylim = c(-1, 1), lwd.baseline = 2,
-  yTicksAt = c(-1.0, 0, 1.0), groups = c("AG", "AL", "DG", "DL"),
+  ylim = c(-1, 1), yTicksAt = c(-1, 0, 1),
+  # baseline = 0, col.baseline = "#838383", lty.baseline = 1, lwd.baseline = 1,
+  groups = c("AG", "AL", "DG", "DL"),
   col = c("#6088C6", "#EF8875", "#49A190", "#ED8D49"), legend = FALSE, 
-  cex.legend = 2, cex.title = 1.5, 
-  fontsize = 9, cex.axis = 1.5, 
-  size = 1.4
+  size = 1
 )
-
-plotTracks(c(axTrack, gmtrack, skipTrack), from = 980, to = 1170)
-
-
 
 ov <- OverlayTrack(
-  list(dummyTrack, gmtrack, pseudoex_splaiTrack, pseudoexonTrack), background.title = "#665990"
+  list(dummyTrack, gmtrack, exonskip_splaiTrack, skipexTrack), 
+  background.title = "#665990"
 )
-plotTracks(c(axTrack, ov), from = 980, to = 1170)
+plotTracks(c(axTrack, ov), from = 985, to = 1155)
 
 
-##
+#3. Whole intron retention
+genemodel <- read.xlsx("03_intret/model_base.xlsx")
+gmtrack <- GeneRegionTrack(
+  genemodel, fill = "#383838",
+  chromosome = chr, genome = "hg19", size = 0.1, alpha = 0.60)
+
+intret <- read.xlsx("03_intret/model_intret.xlsx")
+intretTrack <- GeneRegionTrack(
+  intret, fill = "red",
+  chromosome = chr, genome = "hg19", size = 0.3, alpha = 0.2)
+
+intret_scores <- read.xlsx("03_intret/splai_whole_intret.xlsx")
+intret_splaiTrack <- DataTrack(
+  range = intret_scores, chromosome = chr, genome = gen, name="SpliceAI ∆Score", 
+  background.title = "#F8ACAC", type = "histogram", 
+  ylim = c(-1, 1), yTicksAt = c(-1, 0, 1),
+  baseline = 0, col.baseline = "#838383", lty.baseline = 1, lwd.baseline = 1.0,
+  groups = c("AG", "AL", "DG", "DL"),
+  col = c("#6088C6", "#EF8875", "#49A190", "#ED8D49"), legend = FALSE, 
+  size = 1
+)
+
 ov <- OverlayTrack(
-  list(gmtrack, pseudoex_splaiTrack), background.title = "#665990"
+  list(dummyTrack, gmtrack, intret_splaiTrack, intretTrack), 
+  background.title = "#665990"
 )
-plotTracks(c(axTrack, ov, pseudoexonTrack), from = 980, to = 1170)
+plotTracks(c(axTrack, ov), from = 985, to = 1155)
 
+#4. Partial exon deletion
+genemodel <- read.xlsx("04_partexdel/model_base_exdel.xlsx")
+gmtrack <- GeneRegionTrack(
+  genemodel, fill = "#383838",
+  chromosome = chr, genome = "hg19", size = 0.1, alpha = 0.60)
+
+exdel <- read.xlsx("04_partexdel/model_exdel.xlsx")
+exdelTrack <- GeneRegionTrack(
+  exdel, fill = "#383838",
+  chromosome = chr, genome = "hg19", size = 0.3, alpha = 0.2)
+
+exdel_scores <- read.xlsx("04_partexdel/splai_part_exdel.xlsx")
+exdel_splaiTrack <- DataTrack(
+  range = exdel_scores, chromosome = chr, genome = gen, name="SpliceAI ∆Score", 
+  background.title = "#F8ACAC", type = "histogram", 
+  ylim = c(-1, 1), yTicksAt = c(-1, 0, 1),
+  baseline = 0, col.baseline = "#838383", lty.baseline = 1, lwd.baseline = 1.0,
+  groups = c("AG", "AL", "DG", "DL"),
+  col = c("#6088C6", "#EF8875", "#49A190", "#ED8D49"), legend = FALSE, 
+  size = 1
+)
+
+ov <- OverlayTrack(
+  list(dummyTrack, gmtrack, exdel_splaiTrack, exdelTrack), 
+  background.title = "#838383"
+)
+plotTracks(c(axTrack, ov), from = 985, to = 1155)
+
+
+#5. Partial intron retention
+genemodel <- read.xlsx("05_partintret/model_base.xlsx")
+gmtrack <- GeneRegionTrack(
+  genemodel, fill = "#383838",
+  chromosome = chr, genome = "hg19", size = 0.1, alpha = 0.60)
+
+partintret <- read.xlsx("05_partintret/model_partintret.xlsx")
+partintretTrack <- GeneRegionTrack(
+  partintret, fill = "red",
+  chromosome = chr, genome = "hg19", size = 0.3, alpha = 0.2)
+
+partintret_scores <- read.xlsx("05_partintret/splai_part_intret.xlsx")
+partintret_splaiTrack <- DataTrack(
+  range = partintret_scores, chromosome = chr, genome = gen, name="SpliceAI ∆Score", 
+  background.title = "#F8ACAC", type = "histogram", 
+  ylim = c(-1, 1), yTicksAt = c(-1, 0, 1),
+  baseline = 0, col.baseline = "#838383", lty.baseline = 1, lwd.baseline = 1.0,
+  groups = c("AG", "AL", "DG", "DL"),
+  col = c("#6088C6", "#EF8875", "#49A190", "#ED8D49"), legend = FALSE, 
+  size = 1
+)
+
+ov <- OverlayTrack(
+  list(dummyTrack, gmtrack, partintret_splaiTrack, partintretTrack), 
+  background.title = "#838383"
+)
+plotTracks(c(axTrack, ov), from = 985, to = 1155)
